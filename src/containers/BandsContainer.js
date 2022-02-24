4df1312462f33c380166b1d1e73fbcc8a31b3c1f
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-24 11:51:15 -0500

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
   render() {
+    const bands = this.props.bands.map(band => <li key={band.id}>{band.name}</li>);
     return(
       <div>
-        BandsContainer
+        <strong>BandsContainer</strong>
+        <BandInput addBand={this.props.addBand}/>
+        <ul>
+          {bands}
+        </ul>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return { bands: state.bands }
+};
+
+const mapDispatchToProps = dispatch => {
+  return { addBand: band => dispatch({type: 'ADD_BAND', band})};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer);
+
+
+// ✓ is a child of the app component
+// 7) renders a list element for each band
+// 8) renders each li with the correct name
